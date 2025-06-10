98dd8fa565b7c010c63c6dab727ff1bbc8cda6c2
Barsharani Jena <Barsharanijena555@gmail.com>
2025-06-10 11:30:04 +0530

    codeee

--- Documents/node2505/servers.js
+++ Documents/node2505/servers.js
@@ -4,12 +4,3 @@ var os = require('os');
 var user = os.userInfo();
 console.log(user);
 console.log(user.username);
-
-// Fixed: Added missing comma and corrected file content parameter
-fs.appendFile('greeting.txt', 'Hi ' + user.username + '!\n', (err) => {
-    if (err) {
-        console.log('Error creating file:', err);
-    } else {
-        console.log('File is created');
-    }
-});
